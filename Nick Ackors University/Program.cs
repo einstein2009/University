4d782f2efb861fb3e8d25d851df7e8feb23e10f1
Nick Ackors <einstein_n_2009@yahoo.com>
2016-11-21 22:26:14 -0600

    Committed

--- Nick Ackors University/Program.cs
+++ Nick Ackors University/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// ***********************************************************************
+// Assembly         : University structor
+// Author           : Nick Ackors
+// Created          : 11-20-2016
+//
+// Last Modified By : Nick Ackors
+// Last Modified On : 11-20-2016
+// ***********************************************************************
+// <copyright file="Program.cs" company="">
+//     Copyright ©  2016
+// </copyright>
+// <summary>This program displays the OOP structor of a University.</summary>
+// ***********************************************************************
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +24,205 @@ namespace Nick_Ackors_University
     {
         static void Main(string[] args)
         {
+
+            Staff teacher = new Staff();
+            teacher.Name = "John Snow";
+
+            Console.ReadKey();
+
+            
+            var u = new University();
+            Console.WriteLine(u.Department[2].Staff[3].Name);
+            u.Department[1].Staff[0] = new Researcher("Andy");
+            
+        }
+
+        interface ITeachable
+        {
+            void Teach();
+
+        }
+
+        interface IAdmin
+        {
+            void Administrate();
+        }
+
+
+        //Within a UNIVERSITY there are 4 DEPARTMENTS
+        class University
+        {
+            public University()
+            {
+               
+
+
+                Department[] Department = new Department[4];
+
+                Department[0] = new ;
+                Department[1] = new Department("English");
+                Department[2] = new Geography();
+                Department[3] = new CompScience();
+            }
+        }
+
+        //MATH, ENGLISH, GEOGRAPHY, COMPUTERSCIENCE
+        //Each DEPARTMENT has up to 10 STAFF
+        class Department : Staff
+        {
+            public string[] DeptName { get; set; }
+            public int[] UStaff { get; set; }
+            public string DeptHead { get; set; }
+
+            public Department()
+            {
+
+                Staff[] staff = new Staff[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    staff = new Staff[i];
+                }
+
+                staff[0] = new Dean dean();
+                staff[1] = Professor;
+                staff[2] = Administrator;
+                staff[3] = Researcher;
+
+
+            }
+            
+
+
+
+            
+        }
+
+
+        //There is a DEAN, PROFESSORs, ADMINISTRATORs and RESEARCHERs
+        //Deans, Professors and Researchers should implement an  ITeachable interface which contains the Teach() method
+        //Deans and Administrators should implement an IAdmin interface which contains the Administrate() method
+
+        class Staff
+        {
+            public string Title { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public double Salary { get; set; }
+            public string Name
+            {
+                get { return $"{FirstName} {LastName}"; }
+                set
+                {
+                    string[] names;
+                    names = value.Split(new char[] { ' ' });
+                    if (names.Length == 2)
+                    {
+                        FirstName = names[0];
+                        LastName = names[1];
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Assigned value '{value}' is invalid", "value");
+                    }
+                }
+            }
+
+            //Your constructors should have reasonable parameters to set up the type where you cannot simply use constants.
+            public Staff(string title, string name, double salary)
+            {
+                this.Title = title;
+                this.Name = name;
+                this.Salary = salary;
+            }
+            public Staff()
+            {
+                this.Title = "Default Title";
+                this.Name = "Default Name";
+                this.Salary = 0;
+            }
+
+
+            protected class Dean : ITeachable, IAdmin
+            {
+
+                public void Teach()
+                {
+                    Console.WriteLine("Teach has been called on Dean.");
+                }
+
+                public void Administrate()
+                {
+                    Console.WriteLine("Administrate has been called on Dean.");
+                }
+            }
+            //Professors should have a Class
+            protected class Professor : ITeachable
+            {
+                public string Course { get; set; } = "101";
+
+                public void Teach()
+                {
+                    Console.WriteLine($"{Course} + has been called on Professor.");
+                }
+
+            }
+
+            protected class Administrator : IAdmin
+            {
+
+                public void Teach()
+                {
+                    Console.WriteLine("Teach has been called on Admin.");
+                }
+
+                public void Administrate()
+                {
+                    Console.WriteLine("Administrate has been called on Admin.");
+                }
+            }
+            //Researchers should have a ResearchSpeciality which is a fixed set of research areas represented by an enumeration
+            protected class Researcher : ITeachable
+            {
+
+                public void Teach()
+                {
+                    Console.WriteLine("Teach has been called on Researcher.");
+                }
+
+            }
         }
     }
 }
+
+/*
+1. Create a program with the following OO structure - you do not have to implement any of the methods other than constructors to fill in some default data. You should implement the rest of the methods with a Console.WriteLine to indicate that it has been called.
+
+Within a UNIVERSITY there are 4 DEPARTMENTS
+
+MATH, ENGLISH, GEOGRAPHY, COMPUTERSCIENCE
+
+Each DEPARTMENT has up to 10 STAFF
+
+There is a DEAN, PROFESSORs, ADMINISTRATORs and RESEARCHERs
+
+All Staff should have a name and salary
+
+Professors should have a Class
+
+Researchers should have a ResearchSpeciality which is a fixed set of research areas represented by an enumeration
+
+Deans, Professors and Researchers should implement an  ITeachable interface which contains the Teach() method
+
+Deans and Administrators should implement an  IAdmin interface which contains the Administrate() method
+
+Pay special attention to the IS-A relationships and the HAS-A relationships and implement things correctly.
+
+You should determine reasonable types for all properties not otherwise specified and initialise things with sensible defaults. Your constructors should have reasonable parameters to set up the type where you cannot simply use constants.
+
+var u = new University();
+Console.WriteLine(u.Department[2].Staff[3].Name);
+
+u.Department[1].Staff[0] = new Researcher("Andy");
+
+You should create additional test code to make sure that all of the properties and methods are available
+*/
